1b3702e3199a065c4165811f0b29b7f02fbed80c
Claire Benes <153065643+Bilou4000@users.noreply.github.com>
2024-11-04 14:28:40 +0100

    [init] opengl window creation

--- GLP/OpenGL-Starter/SDL-0/Main.cpp
+++ GLP/OpenGL-Starter/SDL-0/Main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <SDL.h>
 #include <glew.h>
+
 using namespace std;
 //#define GLEW_STATIC
 
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
 	{
 		cout << "SDL initialization succeeded!\n";
 	}
+
 	///////////SETTING UP SDL/////////////
 	//Create a simple window
 	int width = 400;
@@ -33,6 +35,49 @@ int main(int argc, char* argv[])
 		cout << "Glew initialized successfully\n";
 	}
 
-	cin.get();
+	//Set the viewing frame through which we will see the objects
+	glViewport(0, 0, width, height);
+
+	//Put the color you want here for the background
+	glClearColor(0.7f, 0.4f, 0.3f, 1.0f);
+
+
+	//Make a triangle
+	//Describe the shape by its vertices
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	//Create an ID to be given at object generation
+	unsigned int vbo;
+
+	//Pass how many buffers should be created and the reference of the ID to get the value set
+	glGenBuffers(1, &vbo);
+
+
+	bool isRunning = true;
+
+	while (isRunning) {
+		// Inputs
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+		SDL_GL_SwapWindow(Window); // Swapbuffer
+	}
+
+	// Quit
+	SDL_DestroyWindow(Window);
+	SDL_GL_DeleteContext(Context);
+
 	return 0;
 }
